3920499c408f8b7095585dd325855a3358da1aea
flaport <floris.laporte@gmail.com>
2019-11-27 16:42:33 +0100

    changed conflicting keyboard shortcut for copyurl to alt+n

--- config.def.h
+++ config.def.h
@@ -243,7 +243,7 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_u,           kscrollup,      {.i = 10} },
 	{ MODKEY,               XK_d,           kscrolldown,    {.i = 10} },
 	{ MODKEY,               XK_Return,      newterm,        {.i =  0} },
-	{ MODKEY,               XK_l,           copyurl,        {.i =  0} },
+	{ MODKEY,               XK_n,           copyurl,        {.i =  0} },
 	{ MODKEY,               XK_o,           opencopied,     {.v = "xdg-open"} },
 };
 
