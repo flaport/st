1d0fe08abc08921321cb472bd832eb0d7e180ad9
flaport <floris.laporte@gmail.com>
2020-04-26 14:34:20 +0200

    reduce color value for increased transparency

--- x.c
+++ x.c
@@ -681,6 +681,12 @@ void
 xsetalpha(void)
 {
 	dc.col[defaultbg].color.alpha = (unsigned short)(0xffff * alpha);
+	dc.col[defaultbg].color.red =
+		((unsigned short)(dc.col[defaultbg].color.red * alpha)) & 0xff00;
+	dc.col[defaultbg].color.green =
+		((unsigned short)(dc.col[defaultbg].color.green * alpha)) & 0xff00;
+	dc.col[defaultbg].color.blue =
+		((unsigned short)(dc.col[defaultbg].color.blue * alpha)) & 0xff00;
 	dc.col[defaultbg].pixel &= 0x00FFFFFF;
 	dc.col[defaultbg].pixel |= (unsigned char)(0xff * alpha) << 24;
 }
