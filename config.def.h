4e2147a0c3f05668382483e9224442c947fea4e8
flaport <floris.laporte@gmail.com>
2020-06-11 11:26:39 +0200

    change default copyurl keybindings

--- config.def.h
+++ config.def.h
@@ -188,8 +188,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_l,           copyurl,        {.i =  1} },
-	{ MODKEY,               XK_k,           copyurl,        {.i =  -1} },
+	{ MODKEY,               XK_n,           copyurl,        {.i =  +1} },
+	{ MODKEY,               XK_m,           copyurl,        {.i =  -1} },
 };
 
 /*
